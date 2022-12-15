496fdbb1821ce2d1293f0b86ecad569e0201395d
sky81219 <=>
2022-12-15 18:33:15 +0900

    crawling

--- taxi_information/crawling.py
+++ taxi_information/crawling.py
@@ -23,16 +23,15 @@ except FileExistsError:
 
 
 q = Queue()
-log = logger.log()
 month = list(calendar.month_name)
 tlc_url = gd().page()
 
 
-def dd(element: BeautifulSoup) -> List[str]:
+def dirctory_download(element: BeautifulSoup) -> List[str]:
     return [data["href"] for data in element.find_all("a", {"title": "For-Hire Vehicle Trip Records"})]
 
 
-def fd(element: BeautifulSoup) -> None:
+def file_download(element: BeautifulSoup) -> None:
     for i in element.find_all("p"):
         try:
             if month.index(i.text): 
@@ -45,21 +44,23 @@ def search_injection() -> None:
     bs = BeautifulSoup(tlc_url, "html.parser")
     for i in range(22, 19, -1):
         for inner in bs.find_all("div", {"class": "faq-answers", "id": f"faq20{i}"}): 
-            fd(inner)
-            data_struct = dd(inner)
+            file_download(inner)
+            data_struct = dirctory_download(inner)
             q.put(data_struct)
 
 
 def download() -> None:
-    j = 0
+    j: int = 0
     while j < 3:
+        logger.logging.info(f"{j}번째 색션에 접근합니다")
         for da in q.get():
             name: str = da.split("/")[4]
             name_number = int(name.split("_")[2].split("-")[1].split(".")[0])
             file_location = f'{os.getcwd()}/data/{name_number}/{name}'
-            log.info(f"{file_location}/{name_number} 파일로 저장합니다")
+            logger.logging.info(f"{file_location}/{name_number} 파일로 저장합니다")
             urlretrieve(da, file_location)
         j+=1     
+
 
   
 search_injection()
