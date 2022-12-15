8d5f90a5a0ef8711820fc3170cd3b30cf07b623f
sky81219 <=>
2022-12-15 20:04:44 +0900

    crawling

---
+++ taxi_information/size.py
@@ -0,0 +1,14 @@
+import pandas as pd 
+import pathlib
+import os
+import create_log
+from typing import Tuple
+
+burket: int = 0
+for i in (pathlib.Path(f"{os.getcwd()}/data/{i}") for i in range(1, 13)):
+    for j in (i for i in list(pathlib.Path(i).iterdir())):
+        data: int = pd.read_parquet(j).shape[0]
+        burket += data
+
+
+create_log.log().info(f"총 데이터는 : {burket}개 입니다")
